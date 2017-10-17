74bfb5a2f5032e13afc73f061fc6419742cfce14
hadwz <1063141313@qq.com>
2017-10-17 10:31:18 +0800

    resume login

--- Resume-Vue/Resume/src/components/HelloWorld.vue
+++ Resume-Vue/Resume/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
 	<div class="hello">
 		<div class="welcome-section">
 			<div class="imgFrame img-1">
-				<img class="img-circle" src="/static/img/spider.jpg" alt="">
+				<img class="img-circle" v-bind:src=imgSrc.spider1 alt="" @click="goToLogin">
 			</div>
 			<button type="button" class="login-btn animated infinite bounce">
 				<i class="fa fa-5x fa-angle-double-down" aria-hidden="true"  @click="goToLogin"></i>
@@ -17,22 +17,31 @@ import bus from './eventBus.js';
 
 export default {
 	name: 'HelloWorld',
+
 	data () {
 		return {
-			msg: 'Hello World',
+			imgSrc: {
+				spider1: "/static/img/spider.jpg",
+				spider2: "/static/img/spider2.jpg",
+			},
 		}
 	},
-	methods: {
 
+	methods: {
+		//显示登录界面
 		goToLogin: function () {
-			$('#Login').css('top','1rem').animateCss('bounceInUp');
-			$(".img-1 img").attr("src","/static/img/spider2.jpg");
-			$('.login-btn').fadeOut('slow');
+			const Login = $('#Login'),
+				  img = $(".img-1 img"),
+				  btn = $('.login-btn');
+
+			Login.css('top','1rem').animateCss('bounceInUp');
+			img.attr("src",this.imgSrc.spider2).animateCss('flipInY');
+			btn.animateCss('fadeOutDown').fadeOut('slow');
 		},
 
-	},
+	}, //methods end
 	
-}
+};  //default end
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -48,17 +57,16 @@ h1{
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	background-color: #f3fdf5;
+	background-color: #c9e6e2;
 	align-items: center;
-
-	
 }
 .login-btn,.login-btn:hover,.login-btn:visited{
 		background:none;
 		border:none;
-		color:black;
+		color:#f3fdf5;
 		outline: none;
 		align-self: flex-end;
+		
 }
 
 
@@ -73,8 +81,16 @@ h1{
 	max-height: 100%;
 }
 
+.welcome-section{
+	position: relative;
+	margin-top:20rem;
+}
+
 .login-btn{
 	margin-top:2rem;
+	position:absolute;
+	left:3rem;
+	top:8rem;
 }
 
 </style>
--- Resume-Vue/Resume/src/components/Login.vue
+++ Resume-Vue/Resume/src/components/Login.vue
@@ -1,14 +1,34 @@
 <template>
 
 	
-	<section class="row animated" id="Login" v-bind="ctrlShow">
+	<section class="row animated" id="Login" >
 
-		<div class="jumbotron">
-			<h1>Hello, world!</h1>
-			<p>{{ctrlShow.show}}</p>
+		<div class=" col-md-4 col-md-offset-4 " id="loging-form">
+			<div class="img-fly">
+				<img class="img-rounded" v-bind:src=imgSrc.fly alt="">
+			</div>
+			<h3>{{msg}}</h3>
+			 <form class="col-md-8">
+          
+			<div class="input-group">
+				<span class="input-group-addon">
+					<input type="checkbox" aria-label="...">
+				</span>
+				<input type="text" class="form-control" aria-label="...">
+			</div><!-- /input-group -->
+			<div class="input-group">
+				<span class="input-group-addon">
+					<i class="fa fa-1x fa-user-o"></i>
+				</span>
+				<input type="text" class="form-control" aria-label="...">
+			</div><!-- /input-group -->
+
+			</form>
+
 			<p>
 				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 			</p>
+
 		</div>
 
 	</section>
@@ -23,19 +43,16 @@ import bus from './eventBus.js';
 
 	export default {
 		name:'Login',
+
 		data(){
 			return {
-				msg:'from Login',
-				ctrlShow:{
-					show:'',
+				imgSrc: {
+					fly: "/static/img/fly.png"
 				},
+				msg: 'Welcome',
 			}
 		},
-		mounted(){
-			bus.$on('userDefinedEvent',msg=>{
-				this.ctrlShow.show = msg;
-			});
-		},
+		
 	};
 </script>
 
@@ -48,6 +65,34 @@ import bus from './eventBus.js';
 	
 	.user-section,.password-section{
 		margin:2rem auto;
+	}
+
+	#loging-form {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		padding:2rem 2rem;
+		background-color: lightblue;
+		box-shadow: 10px 15px 20px rgba(50,50,50,0.4);
+		background: url('/static/img/timg.jpg') no-repeat;
+		
+	}
+
+	.img-fly{
+		width:60px;
+		height:60px;
+	}
+	.img-fly img{
+		max-width:100%;
+		max-height:100%;
+	}
+
+	.input-group{
+		margin:2rem 0;
+	}
+	h3{
+		color:white;
 	}
 </style>
 
